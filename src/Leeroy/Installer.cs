b2a81b3e70bec268e74cbc0d803a4682241b30ac
Bradley Grainger <bgrainger@gmail.com>
2012-11-07 21:07:06 -0800

    Add service installer.
    The service can be installed using InstallUtil.exe (supplied with the .NET Framework).

---
+++ src/Leeroy/Installer.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace Leeroy
+{
+	[RunInstaller(true)]
+	public partial class Installer : System.Configuration.Install.Installer
+	{
+		public Installer()
+		{
+			InitializeComponent();
+
+			// service account information
+			ServiceProcessInstaller serviceProcessInstaller = new ServiceProcessInstaller
+			{
+				Account = ServiceAccount.LocalService,
+			};
+			Installers.Add(serviceProcessInstaller);
+
+			// service information
+			ServiceInstaller serviceInstaller = new ServiceInstaller
+			{
+				DisplayName = "Leeroy Jenkins Build Service",
+				ServiceName = "Leeroy",
+				StartType = ServiceStartMode.Automatic,
+			};
+			Installers.Add(serviceInstaller);
+		}
+	}
+}
