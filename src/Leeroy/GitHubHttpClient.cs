5264b3753961dd3332638e18bc15d9df290d4950
Bradley Grainger <bgrainger@gmail.com>
2014-02-06 14:43:23 -0800

    Use a CancellationToken with HttpClient.

---
+++ src/Leeroy/GitHubHttpClient.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Octokit;
+using Octokit.Internal;
+
+namespace Leeroy
+{
+	/// <summary>
+	/// Implementation of <see cref="IHttpClient"/> that supports passing a <see cref="CancellationToken"/> to <c>HttpClient.Send</c>.
+	/// </summary>
+	public sealed class GitHubHttpClient : IHttpClient
+	{
+		public GitHubHttpClient(CancellationToken token)
+		{
+			m_token = token;
+			m_httpClientAdapter = new OurHttpClientAdapter();
+		}
+
+		public async Task<IResponse<T>> Send<T>(IRequest request)
+		{
+			var httpOptions = new HttpClientHandler
+			{
+				AllowAutoRedirect = request.AllowAutoRedirect
+			};
+			if (httpOptions.SupportsAutomaticDecompression)
+				httpOptions.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+			var http = new HttpClient(httpOptions)
+			{
+				BaseAddress = request.BaseAddress,
+				Timeout = request.Timeout
+			};
+
+			using (var requestMessage = m_httpClientAdapter.BuildRequestMessage(request))
+			{
+				var responseMessage = await http.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead, m_token).ConfigureAwait(false);
+				return await m_httpClientAdapter.BuildResponse<T>(responseMessage).ConfigureAwait(false);
+			}
+		}
+
+		// We can't derive from HttpClientAdapter because we need to override Send (which is non-virtual), but we can access
+		// the rest of its functionality by using a helper derived class.
+		sealed class OurHttpClientAdapter : HttpClientAdapter
+		{
+			public new HttpRequestMessage BuildRequestMessage(IRequest request)
+			{
+				return base.BuildRequestMessage(request);
+			}
+
+			public new Task<IResponse<T>> BuildResponse<T>(HttpResponseMessage responseMessage)
+			{
+				return base.BuildResponse<T>(responseMessage);
+			}
+		}
+
+		readonly CancellationToken m_token;
+		readonly OurHttpClientAdapter m_httpClientAdapter;
+	}
+}
--- src/Leeroy/Service.cs
+++ src/Leeroy/Service.cs
@@ -21,8 +21,6 @@ namespace Leeroy
 			Log.Info("Initializing service (version {0}).", Assembly.GetExecutingAssembly().GetName().Version);
 
 			ServicePointManager.DefaultConnectionLimit = 10;
-			var gitHubClient = new GitHubClient(new ProductHeaderValue("Leeroy", Program.GetUserAgentVersion()), new InMemoryCredentialStore(new Credentials(Settings.Default.UserName, Settings.Default.Password)), new Uri("http://git/api/v3/"));
-			m_gitHubClient = new GitHubClientWrapper(gitHubClient);
 		}
 
 		internal void Start()
@@ -30,6 +28,16 @@ namespace Leeroy
 			Log.Info("Starting service.");
 
 			m_tokenSource = new CancellationTokenSource();
+
+			var connection = new Connection(
+				new ProductHeaderValue("Leeroy", Program.GetUserAgentVersion()),
+				new Uri("http://git/api/v3/"),
+				new InMemoryCredentialStore(new Credentials(Settings.Default.UserName, Settings.Default.Password)),
+				new GitHubHttpClient(m_tokenSource.Token),
+				new SimpleJsonSerializer());
+			var gitHubClient = new GitHubClient(connection);
+			m_gitHubClient = new GitHubClientWrapper(gitHubClient);
+
 			BuildServerClient buildServerClient = new BuildServerClient(m_tokenSource.Token);
 			Overseer overseer = new Overseer(m_tokenSource.Token, buildServerClient, m_gitHubClient, "Build", "Configuration", "master");
 			m_task = Program.FailOnException(overseer.Run());
@@ -55,6 +63,7 @@ namespace Leeroy
 			// shut down
 			m_task.Dispose();
 			m_tokenSource.Dispose();
+			m_gitHubClient = null;
 		}
 
 		protected override void OnStart(string[] args)
@@ -67,7 +76,7 @@ namespace Leeroy
 			Stop();
 		}
 
-		readonly GitHubClientWrapper m_gitHubClient;
+		GitHubClientWrapper m_gitHubClient;
 		CancellationTokenSource m_tokenSource;
 		Task m_task;
 
